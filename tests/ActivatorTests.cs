d41299ea39f06bda4f522cc7f08490cc771d0b81
bytewizer <58912473+bytewizer@users.noreply.github.com>
2023-09-22 02:04:23 -0700

    Eliminate deprecated asserts  in test cases (#32)
    ***NO_CI***

--- tests/ActivatorTests.cs
+++ tests/ActivatorTests.cs
@@ -16,57 +16,57 @@ namespace nanoFramework.DependencyInjection.UnitTests
         public static void CreateInstance()
         {
             Choice c = (Choice)(Activator.CreateInstance(typeof(Choice)));
-            Assert.Equal(1, c.I);
+            Assert.AreEqual(1, c.I);
 
             c = (Choice)(Activator.CreateInstance(typeof(Choice).FullName));
-            Assert.Equal(1, c.I);
+            Assert.AreEqual(1, c.I);
 
             c = (Choice)(Activator.CreateInstance(typeof(Choice), null));
-            Assert.Equal(1, c.I);
+            Assert.AreEqual(1, c.I);
 
             c = (Choice)(Activator.CreateInstance(typeof(Choice), null, null));
-            Assert.Equal(1, c.I);
+            Assert.AreEqual(1, c.I);
 
             c = (Choice)(Activator.CreateInstance(typeof(Choice), new object[] { }));
-            Assert.Equal(1, c.I);
+            Assert.AreEqual(1, c.I);
 
             c = (Choice)(Activator.CreateInstance(typeof(Choice), new object[] { 42 }));
-            Assert.Equal(2, c.I);
+            Assert.AreEqual(2, c.I);
 
             c = (Choice)(Activator.CreateInstance(typeof(Choice), new object[] { "Hello" }));
-            Assert.Equal(3, c.I);
+            Assert.AreEqual(3, c.I);
 
             c = (Choice)(Activator.CreateInstance(typeof(Choice), new object[] { 5.1, "Hello" }));
-            Assert.Equal(4, c.I);
+            Assert.AreEqual(4, c.I);
         }
 
         [TestMethod]
         public static void CreateInstanceConstructorWithParamsParameter()
         {
             Choice c = (Choice)(Activator.CreateInstance(typeof(Choice), new object[] { new VarArgs(), new object[] { } }));
-            Assert.Equal(5, c.I);
+            Assert.AreEqual(5, c.I);
 
             c = (Choice)(Activator.CreateInstance(typeof(Choice), new object[] { new VarArgs(), new object[] { "P1" } }));
-            Assert.Equal(5, c.I);
+            Assert.AreEqual(5, c.I);
 
             c = (Choice)(Activator.CreateInstance(typeof(Choice), new object[] { new VarArgs(), new object[] { "P1", "P2" } }));
-            Assert.Equal(5, c.I);
+            Assert.AreEqual(5, c.I);
 
             c = (Choice)(Activator.CreateInstance(typeof(Choice), new object[] { new VarStringArgs(), new string[] { } }));
-            Assert.Equal(6, c.I);
+            Assert.AreEqual(6, c.I);
 
             c = (Choice)(Activator.CreateInstance(typeof(Choice), new object[] { new VarStringArgs(), new string[] { "P1" } }));
-            Assert.Equal(6, c.I);
+            Assert.AreEqual(6, c.I);
 
             // TODO:? c = (Choice1)(Activator.CreateInstance(typeof(Choice1), new object[] { new VarStringArgs(), "P1", "P2" }));
             c = (Choice)(Activator.CreateInstance(typeof(Choice), new object[] { new VarStringArgs(), new string[] { "P1", "P2" } }));
-            Assert.Equal(6, c.I);
+            Assert.AreEqual(6, c.I);
         }
 
         [TestMethod]
         public void CreateInstanceNullTypeThrowsArgumentNullException()
         {
-            Assert.Throws(typeof(ArgumentNullException), () => Activator.CreateInstance(null, new object[0]));
+            Assert.ThrowsException(typeof(ArgumentNullException), () => Activator.CreateInstance(null, new object[0]));
         }
 
         public class Choice
