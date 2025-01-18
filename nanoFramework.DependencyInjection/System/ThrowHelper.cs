a71d4f8748e21c9f032cdfb2cf681321e60c2be9
Cory Charlton <CoryCharlton@users.noreply.github.com>
2025-01-17 21:19:53 -0800

    Fixing missing dependency in nuspec

--- nanoFramework.DependencyInjection/System/ThrowHelper.cs
+++ nanoFramework.DependencyInjection/System/ThrowHelper.cs
@@ -58,18 +58,3 @@ namespace System
     }
 }
 
-// TODO: Remove if my PR gets merged into System.Runtime
-namespace System.Runtime.CompilerServices
-{
-    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
-    internal sealed class CallerArgumentExpressionAttribute : Attribute
-    {
-        public CallerArgumentExpressionAttribute(string parameterName)
-        {
-            ParameterName = parameterName;
-        }
-
-        public string ParameterName { get; }
-    }
-}
-
